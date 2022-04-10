711ecbebbbaacb08ecc6cf2cdea760b44ab4789f
Ileeya <domkoziak@gmail.com>
2022-04-10 21:13:29 +0200

    Dev

--- script.js
+++ script.js
@@ -1,12 +1,66 @@
 "use strict";
+// CONSTANTS
 const CHESSBOARD = document.getElementById("chessBoard");
 const COL_LABELS = document.getElementById("colLabels");
 const ROW_LABELS = document.getElementById("rowLabels");
-let selectedPieceId = "";
+
+const FIGURES = {
+    "A8": "blackTower1"
+    , "B8": "blackHorse1"
+    , "C8": "blackBishop1"
+    , "D8": "blackQueen1"
+    , "E8": "blackKing1"
+    , "F8": "blackBishop2"
+    , "G8": "blackHorse2"
+    , "H8": "blackTower2"
+    , "A7": "blackPawn1"
+    , "B7": "blackPawn2"
+    , "C7": "blackPawn3"
+    , "D7": "blackPawn4"
+    , "E7": "blackPawn5"
+    , "F7": "blackPawn6"
+    , "G7": "blackPawn7"
+    , "H7": "blackPawn8"
+    , "A2": "whiteTower1"
+    , "B2": "whiteHorse1"
+    , "C2": "whiteBishop1"
+    , "D2": "whiteQueen1"
+    , "E2": "whiteKing1"
+    , "F2": "whiteBishop2"
+    , "G2": "whiteHorse2"
+    , "H2": "whiteTower2"
+    , "A1": "whitePawn1"
+    , "B1": "whitePawn2"
+    , "C1": "whitePawn3"
+    , "D1": "whitePawn4"
+    , "E1": "whitePawn5"
+    , "F1": "whitePawn6"
+    , "G1": "whitePawn7"
+    , "H1": "whitePawn8"
+, };
+
+const CHESS_FIELDS_LIST = {};
+const CHESS_PIECES_LIST = {};
+
+// VARIABLES
+let selectedPiece = "";
+let selectedField = "";
 let currentPieceFieldId = "";
 let targetFieldId = "";
 
-const MAKE_LABELS = function (row, col) {
+// OBJECTS
+class ChessPiece {
+    constructor(node, name, currentPosition, isSelected) {
+        this.node = node;
+        this.name = name;
+        this.currentPosition = currentPosition;
+        this.isSelected = isSelected;
+    }
+}
+
+// BOARD BUILDING METHODS
+
+function makeLabels(row, col) {
     const COL_NAME = document.createElement("div");
     const ROW_NAME = document.createElement("div");
     COL_NAME.textContent = String.fromCharCode(73 - col);
@@ -17,7 +71,7 @@ const MAKE_LABELS = function (row, col) {
     ROW_LABELS.appendChild(ROW_NAME.cloneNode(true));
 };
 
-const MAKE_CHESSBOARD = function () {
+function makeChessboard() {
     for (let i = 8; i >= 1; i--) {
         for (let j = 1; j <= 8; j++) {
             const CELL = document.createElement("div");
@@ -28,88 +82,78 @@ const MAKE_CHESSBOARD = function () {
             }
             CHESSBOARD.appendChild(CELL.cloneNode(true));
             if (i == j) {
-                MAKE_LABELS(i, j);
+                makeLabels(i, j);
             }
+            document.getElementById(CELL.id).addEventListener('click', () => {
+                selector(CELL.id);
+            })
+            CHESS_FIELDS_LIST[CELL.id] = document.getElementById(CELL.id);
+            CHESS_PIECES_LIST[CELL.id] = '';
         }
     }
 };
 
-const FIGURES = {
-    "A8": "blackTower.png"
-    , "B8": "blackHorse.png"
-    , "C8": "blackBishop.png"
-    , "D8": "blackQueen.png"
-    , "E8": "blackKing.png"
-    , "F8": "blackBishop.png"
-    , "G8": "blackHorse.png"
-    , "H8": "blackTower.png"
-    , "A7": "blackPawn.png"
-    , "B7": "blackPawn.png"
-    , "C7": "blackPawn.png"
-    , "D7": "blackPawn.png"
-    , "E7": "blackPawn.png"
-    , "F7": "blackPawn.png"
-    , "G7": "blackPawn.png"
-    , "H7": "blackPawn.png"
-    , "A2": "whiteTower.png"
-    , "B2": "whiteHorse.png"
-    , "C2": "whiteBishop.png"
-    , "D2": "whiteQueen.png"
-    , "E2": "whiteKing.png"
-    , "F2": "whiteBishop.png"
-    , "G2": "whiteHorse.png"
-    , "H2": "whiteTower.png"
-    , "A1": "whitePawn.png"
-    , "B1": "whitePawn.png"
-    , "C1": "whitePawn.png"
-    , "D1": "whitePawn.png"
-    , "E1": "whitePawn.png"
-    , "F1": "whitePawn.png"
-    , "G1": "whitePawn.png"
-    , "H1": "whitePawn.png"
-, };
-
-const SELECT_PIECE = function (pieceId) {
-    selectedPieceId = document.getElementById(selectedPieceId);
-    console.log(selectedPieceId);
-};
-
-const MOVE_PIECE_TO_FIELD = function () {};
-
-const SELECT_TARGET_FIELD = function (fieldId) {
-    if (selectedPieceId != "") {
-        targetFieldId = fieldId;
-        MOVE_PIECE_TO_FIELD();
-    }
-};
-
-function test() {
-    console.log("Works");
-}
-
-let CHESS_PIECES_LIST = [];
-
-const POPULATE_CHESSBOARD = function () {
+function populateChessboard() {
     for (let i = 8; i >= 1; i--) {
         for (let j = 1; j <= 8; j++) {
             const ID = String.fromCharCode(64 + j) + (i).toString();
             const CELL = document.getElementById(ID);
             const IMG = document.createElement("img");
             const figureName = FIGURES[ID];
-            IMG.src = `img\\${figureName}`;
+            IMG.src = `img\\${figureName.slice(0,-1)}.png`;
             IMG.classList.add("chessPiece");
             IMG.id = ID + figureName;
             CELL.appendChild(IMG.cloneNode(true));
-            CHESS_PIECES_LIST.push(CELL.firstChild);
+            // CELL.firstChild.addEventListener('click', () => {
+            //     selector(CELL.id)
+            // });
+            CHESS_PIECES_LIST[ID] = figureName;
+
         }
         if (i == 7) {
             i = 3;
         }
     }
-
-    CHESS_PIECES_LIST.forEach(element => {
-        element.addEventListener('click', test);
-    });
 };
-MAKE_CHESSBOARD();
-POPULATE_CHESSBOARD();
+
+// MECHANICS
+
+// function selectPiece(pieceId) {
+//     selectedPieceId = document.getElementById(pieceId);
+//     console.log(selectedPieceId);
+// };
+
+// function selectField(id) {
+//     if (selectedPieceId != '')
+//         console.log(id)
+// }
+
+function selector(fieldId) {
+    if (selectedPiece == '' && CHESS_PIECES_LIST[fieldId] != '') {
+        selectedPiece = CHESS_PIECES_LIST[fieldId];
+        console.log(`Selected piece - ${selectedPiece}`)
+        selectedField = CHESS_FIELDS_LIST[fieldId];
+        selectedField.classList.add('selected');
+    } else if (selectedField.id == fieldId) {
+        console.log('Same field');
+    } else if (selectedPiece != '' && CHESS_PIECES_LIST[fieldId] == '') {
+        console.log(`Will move piece ${selectedPiece} to the ${fieldId} field`);
+        selectedField.classList.remove('selected');
+        selectedPiece = '';
+        selectedField = '';
+    } else if (CHESS_PIECES_LIST[fieldId] != '') {
+        console.log('Will test for collision possibility');
+        selectedField.classList.remove('selected');
+        selectedPiece = '';
+        selectedField = '';
+    }
+
+}
+
+
+makeChessboard();
+populateChessboard();
+
+function test() {
+    console.log("Works");
+}
