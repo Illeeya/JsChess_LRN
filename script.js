18d41ae0ab6eaa3e0a71e9c18d94671a83bd44d0
Ileeya <domkoziak@gmail.com>
2022-04-25 23:12:29 +0200

    DEV 2504222311 - Figures move freely

--- script.js
+++ script.js
@@ -21,22 +21,22 @@ const FIGURES = {
     , F7: 'blackPawn6'
     , G7: 'blackPawn7'
     , H7: 'blackPawn8'
-    , A2: 'whiteTower1'
-    , B2: 'whiteHorse1'
-    , C2: 'whiteBishop1'
-    , D2: 'whiteQueen1'
-    , E2: 'whiteKing1'
-    , F2: 'whiteBishop2'
-    , G2: 'whiteHorse2'
-    , H2: 'whiteTower2'
-    , A1: 'whitePawn1'
-    , B1: 'whitePawn2'
-    , C1: 'whitePawn3'
-    , D1: 'whitePawn4'
-    , E1: 'whitePawn5'
-    , F1: 'whitePawn6'
-    , G1: 'whitePawn7'
-    , H1: 'whitePawn8'
+    , A1: 'whiteTower1'
+    , B1: 'whiteHorse1'
+    , C1: 'whiteBishop1'
+    , D1: 'whiteQueen1'
+    , E1: 'whiteKing1'
+    , F1: 'whiteBishop2'
+    , G1: 'whiteHorse2'
+    , H1: 'whiteTower2'
+    , A2: 'whitePawn1'
+    , B2: 'whitePawn2'
+    , C2: 'whitePawn3'
+    , D2: 'whitePawn4'
+    , E2: 'whitePawn5'
+    , F2: 'whitePawn6'
+    , G2: 'whitePawn7'
+    , H2: 'whitePawn8'
 , };
 
 const CHESS_FIELDS_LIST = {};
@@ -97,17 +97,8 @@ function populateChessboard() {
     for (let i = 8; i >= 1; i--) {
         for (let j = 1; j <= 8; j++) {
             const ID = String.fromCharCode(64 + j) + i.toString();
-            const CELL = document.getElementById(ID);
-            const IMG = document.createElement('img');
-            const figureName = FIGURES[ID];
-            IMG.src = `img\\${figureName.slice(0, -1)}.png`;
-            IMG.classList.add('chessPiece');
-            IMG.id = ID + figureName;
-            CELL.appendChild(IMG.cloneNode(true));
-            // CELL.firstChild.addEventListener('click', () => {
-            //     selector(CELL.id)
-            // });
-            CHESS_PIECES_LIST[ID] = figureName;
+            const _figureName = FIGURES[ID];
+            putPieceOnCell(ID, _figureName);
         }
         if (i == 7) {
             i = 3;
@@ -115,17 +106,24 @@ function populateChessboard() {
     }
 }
 
+function putPieceOnCell(cellId, figureName) {
+    const CELL = document.getElementById(cellId);
+    const IMG = document.createElement('img');
+    IMG.src = `img\\${figureName.slice(0, -1)}.png`;
+    IMG.classList.add('chessPiece');
+    IMG.id = cellId + figureName;
+    CELL.appendChild(IMG.cloneNode(true));
+    CHESS_PIECES_LIST[cellId] = figureName;
+}
+
 // MECHANICS
 
-// function selectPiece(pieceId) {
-//     selectedPieceId = document.getElementById(pieceId);
-//     console.log(selectedPieceId);
-// };
-
-// function selectField(id) {
-//     if (selectedPieceId != '')
-//         console.log(id)
-// }
+function movePiece(targetFieldId) {
+    CHESS_PIECES_LIST[selectedField.id] = '';
+    putPieceOnCell(targetFieldId, selectedPiece);
+    const _selectedFieldChild = selectedField.firstChild;
+    selectedField.removeChild(_selectedFieldChild);
+}
 
 function selector(fieldId) {
     if (selectedPiece == '' && CHESS_PIECES_LIST[fieldId] != '') {
@@ -136,10 +134,13 @@ function selector(fieldId) {
     } else if (selectedField.id == fieldId) {
         console.log('Same field');
     } else if (selectedPiece != '' && CHESS_PIECES_LIST[fieldId] == '') {
+        targetFieldId = fieldId;
+        movePiece(fieldId);
         console.log(`Will move piece ${selectedPiece} to the ${fieldId} field`);
         selectedField.classList.remove('selected');
         selectedPiece = '';
         selectedField = '';
+        targetFieldId = '';
     } else if (CHESS_PIECES_LIST[fieldId] != '') {
         console.log('Will test for collision possibility');
         selectedField.classList.remove('selected');
